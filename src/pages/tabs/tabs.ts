8f3b69bdefd796d9384163f8c58aca7aa06e87e1
benwingz <benjamin.roullet@gmail.com>
2017-02-27 17:03:37 +0100

    :sparkles: Add facebook authentication

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -8,6 +8,8 @@ import { UserService } from '../../service/user.service';
 
 import { Storage } from '@ionic/storage';
 
+import { FacebookAuth, User } from '@ionic/cloud-angular';
+
 @Component({
   templateUrl: 'tabs.html'
 })
@@ -20,7 +22,9 @@ export class TabsPage implements OnInit {
 
   constructor(
     private UserService: UserService,
-    private storage: Storage
+    private storage: Storage,
+    public facebookAuth: FacebookAuth,
+    public user: User
   ) { }
 
   getToken(): void {
@@ -32,7 +36,26 @@ export class TabsPage implements OnInit {
   }
 
   ngOnInit(): void {
-    this.getToken();
+    //this.getToken();
+    this.facebookAuth.login().then(res => this.initLogin(res));
+  }
+
+  private initLogin(facebookResponse): void {
+    console.log('User', this.user);
+    this.storage.remove('email');
+    this.storage.remove('cloud_id');
+    this.storage.remove('fb_id');
+    this.storage.remove('fb_profile_picture');
+    this.storage.set('email', this.user.social.facebook.data.email);
+    this.storage.set('cloud_id', this.user.id);
+    this.storage.set('fb_id', this.user.social.facebook.uid);
+    this.storage.set('fb_profile_picture', this.user.social.facebook.data.profile_picture);
+    this.storage.set('fb_fullname', this.user.social.facebook.data.full_name);
+    this.UserService.getToken({email: this.user.social.facebook.data.email, cloud_id: this.user.id, fb_id: this.user.social.facebook.uid, fb_full_name: this.user.social.facebook.data.email}).then( (res) => {
+      console.log(res);
+      this.storage.remove('token');
+      this.storage.set('token', res.token);
+    })
   }
 
 }
