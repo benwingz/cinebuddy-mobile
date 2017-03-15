911c6364de6eaee602336b1a394e5ace40b62e80
ben <b.roullet@skilvioo.com>
2017-03-15 20:08:49 +0100

    :lipstick: Add buttons to switch movies

--- src/component/movielist/movielist.component.ts
+++ src/component/movielist/movielist.component.ts
@@ -1,8 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, HostListener } from '@angular/core';
+import { Slides } from 'ionic-angular';
 
 @Component({
   selector: 'movie-list-cmp',
   template: `
+  <div class="controls">
+    <ion-icon name="arrow-dropleft-circle" (click)="goBack()"></ion-icon>
+    <ion-icon name="arrow-dropright-circle" (click)="goForward()"></ion-icon>
+  </div>
   <ion-slides>
     <ion-slide *ngFor="let movie of movies">
       <movie-cmp  [movie]="movie"></movie-cmp>
@@ -12,6 +17,23 @@ import { Component, Input } from '@angular/core';
 export class MovieListCmp {
 
   @Input('movieList') movies:any;
+  @ViewChild(Slides) slides:Slides;
 
   constructor(){}
+
+  @HostListener('window:scroll', [])
+  onScroll() {
+    console.log(window.pageYOffset);
+  }
+
+  goBack(): void {
+    /*if (this.slides.getActiveIndex() != 0){
+
+    }*/
+    this.slides.slidePrev();
+  }
+
+  goForward(): void {
+    this.slides.slideNext();
+  }
 }
